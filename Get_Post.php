086fd14db6932c0d2b2359215c8c12eac0d97c8b
Sahil pillania <80634110+Sahil-pillania@users.noreply.github.com>
2021-05-21 10:42:12 +0530

    Update Get_Post.php

--- Get_Post.php
+++ Get_Post.php
@@ -65,7 +65,7 @@
 
 <div class="container mt-3">
 <h1>Please enter your email and password</h1>
-    <form action="/" method="post">
+    <form action="PHP/Get_Post.php" method="post">
     <div class="form-group">
         <label for="email">Email address</label>
         <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
